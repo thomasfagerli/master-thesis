2706e3902a37b55b7c194794b462a3dda5cc5d1f
unknown <thomaf0107@yahoo.no>
2025-02-15 20:01:49 +0100

    added json to csv

--- raspi/handle_sheet.py
+++ raspi/handle_sheet.py
@@ -1,13 +1,41 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import csv
+import io
 
+#convert json to csv
+def json_to_csv(data):
+    # Extract relevant values
+    row = [
+        data['timestamp'],
+        data['download'],
+        data['upload'],
+        data['ping'],
+        data['server']['lat'],
+        data['server']['lon'],
+        data['server']['name'],
+        data['server']['country'],
+        data['server']['sponsor'],
+        data['server']['id'],
+        data['server']['latency'],
+        data['share'],
+        data['client']['lat'],
+        data['client']['lon']
+    ]
 
+    # Write to a string buffer
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(row)
+
+    return output.getvalue()
+
+#get service
 def get_service(spreadsheet_id):
     credentials = service_account.Credentials.from_service_account_file("key.json", scopes=["https://www.googleapis.com/auth/spreadsheets"])
     service = build("sheets", "v4", credentials=credentials)
     return service
-
 
 #append a row to the sheet from a csv string
 def append_row(csv_string, service, spreadsheet_id, sheet_name): 
--- raspi/sample.py
+++ raspi/sample.py
@@ -1,7 +1,7 @@
 import speedtest
 from datetime import datetime
 import time
-from handle_sheet import append_row, get_service, create_sheet
+from handle_sheet import append_row, get_service, create_sheet, json_to_csv
 
 
 #This function runs a speedtest and returns the results
@@ -12,7 +12,7 @@ def run_speedtest():
     s.download()
     s.upload()
     s.results.share()
-    return s.results.csv()  #returns a csv string of the results    
+    return s.results.dict()     
 
 
 
@@ -20,14 +20,16 @@ spreadsheet_id = "1mFNR6EdvY22x0fUlCELVniT_OYJgioKsR0TJSCOYino"
 
 
 
-sheet_name = datetime.now().strftime("%Y-%m-%d_%H-%M") 
+sheet_name = datetime.now().strftime("%Y.%m.%d %H:%M") 
 create_sheet(sheet_name, get_service(spreadsheet_id), spreadsheet_id)
 print(f"Creating new sheet: {sheet_name}")
+header = "timestamp,download,upload,ping,server_lat,server_lon,server_name,server_country,server_sponsor,server_id,server_latency,share_url,client_lat,client_lon"
+append_row(header, get_service(spreadsheet_id), spreadsheet_id, sheet_name)
 
 #run the speedtest every 60 seconds
 while True:
     try:
-        results = run_speedtest()
+        results = json_to_csv(run_speedtest())
         if results:
             print(results)
             append_row(results, get_service(spreadsheet_id), spreadsheet_id, sheet_name)
